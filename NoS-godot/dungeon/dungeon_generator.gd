b2d7a0bb3d52b07a36b36a4fe0a458d302cfbcfc
Monique <otavioschimith124@gmail.com>
2025-08-21 22:30:01 -0300

    fix

--- NoS-godot/dungeon/dungeon_generator.gd
+++ NoS-godot/dungeon/dungeon_generator.gd
@@ -4,8 +4,6 @@ func _init() -> void:
 	LoadMapNodes.load_map_nodes()
 
 func generate_dungeon(dungeon_type: Tables.DungeonType = -1, level := -1):
-	var room_size = Game.ROOM_SIZE
-	
 	if (dungeon_type == -1):
 		dungeon_type = randi_range(0, Tables.DungeonType.size() - 1)
 	
@@ -14,32 +12,36 @@ func generate_dungeon(dungeon_type: Tables.DungeonType = -1, level := -1):
 	var ambient := {
 		"dungeon_table": dungeon_table,
 		"rooms_amount": dungeon_table.rooms_amount,
-		offsets: [
+		"offsets": [
 			Vector2i.UP,
 			Vector2i.DOWN,
 			Vector2i.RIGHT,
 			Vector2i.LEFT,
 		],
-		offset_letter: [
+		"offset_letter": [
 			"U",
 			"D",
 			"R",
 			"L"
 		],
-		to_collapse: [],
-		nodes: [],
-		salas: 1,
-		node_grid: [],
-		roomsWidth: obj_dungeon_manager.width div roomSize,
-		roomsHeight: obj_dungeon_manager.height div roomSize,
-		init_x: -1,
-		init_y: -1
+		"to_collapse": [],
+		"nodes": [],
+		"salas": 1,
+		"node_grid": [],
+		"rooms_width": Game.WIDTH / Game.ROOM_SIZE,
+		"rooms_height": Game.HEIGHT / Game.ROOM_SIZE,
+		"init_x": -1,
+		"init_y": -1
 	}
 	
-	collapse()
+	collapse(ambient)
 
-func collapse():
-	pass
+func collapse(a: Dictionary[String, Variant]):
+	a["to_collapse"].append(Vector2i(a["rooms_width"] / 2, a["rooms_height"] / 2))
+	var initial = true
+	
+	while (a["to_collapse"].size() > 0):
+		pass
 
-func isInside(point: Vector2i):
-	return (point.x >= 0 && point.x < roomsWidth && point.y >= 0 && point.y < roomsHeight)
+func isInside(point: Vector2i, a: Dictionary):
+	return (point.x >= 0 && point.x < a["rooms_width"] && point.y >= 0 && point.y < a["rooms_height"])
